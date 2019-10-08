9101c53c848c1b4803e6aebb00540a83843a694e
Chris Davies <c.m.davies10@gmail.com>
2019-10-08 14:21:34 -0400

    working buttons with gifs pulling onto page

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-var search = "skunk";
-var limit = 10;
-var apiKey = "Kfdsg3pgJEX2b3rt0Ofr1yrjNig3NFli";
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=" + limit;
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk"];
+var search;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-})
+
+window.onload = function () {
+    for (var i = 0; i < animals.length; i++) {
+        var a = $("<button>")
+        a.text(animals[i]);
+        a.addClass("animalButton btn btn-lg btn-primary");
+        a.attr("value", animals[i]);
+        $("#buttons").append(a);
+    };
+};
+
+$(document).ready(function () {
+    $(".animalButton").on("click", function () {
+        // event.preventDefault();
+        // alert("this is working");
+        var search = $(this).val().trim();
+        var limit = 10;
+        var apiKey = "Kfdsg3pgJEX2b3rt0Ofr1yrjNig3NFli";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=" + limit;
+
+        console.log($(this).val().trim());
+        
+        console.log("===");
+        console.log(search);
+
+        $("#gifs-here").empty();
+        
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+            console.log(response.data[0].url)
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var animalDIV = $("<div>");
+                var animalImage = $("<img>");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                animalDIV.append(animalImage);
+                $("#gifs-here").append(animalDIV);
+
+            };
+
+
+            
+        });
+   });
+
+    $("#add-button").on("click", function (event) {
+        event.preventDefault();
+        var newButton = $(this).val().trim();
+        console.log($(this).val().trim());
+        var b = $("<button>")
+        b.addClass("animalButton btn btn-lg btn-primary");
+        b.attr("value", newButton);
+        $("#buttons").append(b);
+
+
+    })
+    
+ });
